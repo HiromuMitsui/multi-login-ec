0c78efa4d731fafc475c419e25a295f56359335d
tauromu <tauromu166@gmail.com>
2022-11-03 21:20:55 +0900

    component initail value

--- src/multi-login-ec/resources/views/components/tests/card.blade.php
+++ src/multi-login-ec/resources/views/components/tests/card.blade.php
@@ -1,3 +1,5 @@
+@props(['title', 'message' => 'init value.', 'content' => 'init sentense'])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{ $title }}</div>
     <div>image</div>
--- src/multi-login-ec/resources/views/tests/component-test1.blade.php
+++ src/multi-login-ec/resources/views/tests/component-test1.blade.php
@@ -2,4 +2,5 @@
     <x-slot name="header">header1</x-slot>
     component test
     <x-tests.card title="title1" content="sentence" :message="$message" />
+    <x-tests.card title="title1" />
 </x-tests.app>
