af3d3b517688b17370b654856b3428b1ffe0943d
tauromu <tauromu166@gmail.com>
2022-11-03 21:26:47 +0900

    attributes merge

--- src/multi-login-ec/resources/views/components/tests/card.blade.php
+++ src/multi-login-ec/resources/views/components/tests/card.blade.php
@@ -1,6 +1,8 @@
 @props(['title', 'message' => 'init value.', 'content' => 'init sentense'])
 
-<div class="border-2 shadow-md w-1/4 p-2">
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-md w-1/4 p-2'
+    ]) }}>
     <div>{{ $title }}</div>
     <div>image</div>
     <div>{{ $content }}</div>
