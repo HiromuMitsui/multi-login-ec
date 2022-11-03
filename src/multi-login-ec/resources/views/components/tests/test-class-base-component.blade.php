a502fae1f9059277639d6fcae6da62c9153b1c02
tauromu <tauromu166@gmail.com>
2022-11-03 21:44:47 +0900

    classbase component

---
+++ src/multi-login-ec/resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+    This is a class based component.
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+</div>
--- src/multi-login-ec/resources/views/tests/component-test2.blade.php
+++ src/multi-login-ec/resources/views/tests/component-test2.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
     <x-slot name="header">header2</x-slot>
+    <x-test-class-base classBaseMessage="This is a message." />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="This is a message." defaultMessage="Changed Message." />
 </x-tests.app>
