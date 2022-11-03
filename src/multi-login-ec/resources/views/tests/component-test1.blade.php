ada2337554320613ff7ad50323fc983e8e2ada9d
tauromu <tauromu166@gmail.com>
2022-11-03 20:33:52 +0900

    component valueable

--- src/multi-login-ec/resources/views/tests/component-test1.blade.php
+++ src/multi-login-ec/resources/views/tests/component-test1.blade.php
@@ -1,5 +1,5 @@
 <x-tests.app>
     <x-slot name="header">header1</x-slot>
     component test
-    <x-tests.card title="title1" content="sentence" />
+    <x-tests.card title="title1" content="sentence" :message="$message" />
 </x-tests.app>
